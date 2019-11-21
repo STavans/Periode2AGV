a1e9109374c54170ff424f2e4fc82e0de76cdcdb
MickWerf <mickwerf@outlook.com>
2019-11-21 15:22:29 +0100

    Edited LED interface, individualLed, LedGroup and NeoPixel to match latest version after merge.

--- src/avg1a2/Project/hardware/signal/led/LED.java
+++ src/avg1a2/Project/hardware/signal/led/LED.java
@@ -2,10 +2,12 @@ package avg1a2.project.hardware.signal.led;
 
 import avg1a2.project.hardware.Component;
 
+/**
+ * Interface describes what led objects need to contain.
+ */
 public interface LED extends Component {
     void on();
     void off();
     void setDelay(int delay);
-    void setOffSet(int offSet);
-    void setBrightness(int brightness);
+    void setOffset(int offSet);
 }
