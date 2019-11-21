24a36d9474ed64e9dd83374239d26c7066a1380b
MickWerf <mickwerf@outlook.com>
2019-11-21 12:15:44 +0100

    Updated interface Component with an update function Updated LED to extend Component and added LED specefic functions Updated Sensor to extend Component and added Sensor IsActive function

--- src/avg1a2/Project/engine/LED.java
+++ src/avg1a2/Project/engine/LED.java
@@ -1,4 +1,9 @@
 package avg1a2.project.engine;
 
-public interface LED {
+public interface LED extends Component {
+    void on();
+    void off();
+    void setDelay(int delay);
+    void setOffSet(int offSet);
+    void setBrightness(int brightness);
 }
