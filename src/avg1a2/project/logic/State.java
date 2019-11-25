d86ea84ed50e0c5695ee7b69a755311a83f357d4
MickWerf <mickwerf@outlook.com>
2019-11-25 13:48:48 +0100

    Added a setState function in State with error handling

--- src/avg1a2/project/logic/State.java
+++ src/avg1a2/project/logic/State.java
@@ -5,14 +5,23 @@ public class State {
     private String routing;
     private boolean overwritten;
 
-    public State() {
+    State() {
         this.override = "Override";
         this.routing = "Routing";
         this.overwritten = false;
     }
 
+    public void setState(String state) throws IllegalArgumentException {
+        if (state.equals("Override")) {
+            this.overwritten = true;
+        } else if (state.equals("Routing")) {
+            this.overwritten = false;
+        } else {
+            throw new IllegalArgumentException("Not a valid state.");
+        }
+    }
 
-    public String getState() {
+    String getState() {
         if (overwritten) {
             return override;
         } else {
