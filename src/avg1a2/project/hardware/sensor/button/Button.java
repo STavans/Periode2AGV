bd6b5da6918865bb469484bd06592610d88c839c
sebas <mab.tromper@student.avans.nl>
2019-11-25 13:23:47 +0100

    no message

--- src/avg1a2/project/hardware/sensor/button/Button.java
+++ src/avg1a2/project/hardware/sensor/button/Button.java
@@ -1,19 +1,22 @@
-package avg1a2.project.hardware.sensor;
+package avg1a2.project.hardware.sensor.button;
 
 import TI.BoeBot;
+import avg1a2.project.hardware.sensor.Sensor;
 
 /**
  * A button is a sensor based object which will return true when pressed and false when not.
  */
 public class Button implements Sensor {
+    private ButtonCallback buttonCallBack;
     private int button;
 
     /**
      * Constructor which sets the pin to read button status.
-     * @param input pin on which the button is connected.
+     * @param pin pin on which the button is connected.
      */
-    public Button(int input) {
-        this.button = input;
+    public Button(int pin, ButtonCallback buttonCallBack) {
+        this.button = pin;
+        this.buttonCallBack = buttonCallBack;
     }
 
     /**
@@ -26,6 +29,8 @@ public class Button implements Sensor {
 
     @Override
     public void update() {
-
+        if(this.isActive()){
+            buttonCallBack.onButtonPress();
+        }
     }
 }
--- src/avg1a2/project/hardware/sensor/button/ButtonCallback.java
+++ src/avg1a2/project/hardware/sensor/button/ButtonCallback.java
@@ -1,4 +1,5 @@
 package avg1a2.project.hardware.sensor.button;
 
 public interface ButtonCallback {
+    void onButtonPress();
 }
