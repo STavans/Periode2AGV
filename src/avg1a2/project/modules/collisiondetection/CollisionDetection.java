6e497172abf191b5e1a75d520a5a9c02c1fb4a77
sebas <mab.tromper@student.avans.nl>
2019-11-26 15:19:21 +0100

    no message

--- src/avg1a2/project/modules/collisiondetection/CollisionDetection.java
+++ src/avg1a2/project/modules/collisiondetection/CollisionDetection.java
@@ -1,4 +1,40 @@
 package avg1a2.project.modules.collisiondetection;
 
-public class CollisionDetection {
+import avg1a2.project.hardware.sensor.ultrasonic.UltraSonicCallback;
+import avg1a2.project.hardware.sensor.ultrasonic.UltrasonicSensor;
+import avg1a2.project.hardware.signal.Speaker;
+import avg1a2.project.hardware.signal.led.LedGroup;
+import avg1a2.project.hardware.signal.led.NeoPixel;
+import avg1a2.project.modules.controller.MotionControl;
+
+public class CollisionDetection implements UltraSonicCallback {
+
+    private CollisionDetectionCallback collisionDetectionCallback;
+
+    public CollisionDetection(CollisionDetectionCallback collisionDetection){
+        this.collisionDetectionCallback = collisionDetection;
+    }
+
+    /**
+     *
+     */
+    public void update(){
+        UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(0, 1, this, true);
+        while (true) {
+            ultrasonicSensor.update();
+        }
+    }
+
+    public void collisionWhisker(){
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void onUltraSonic() {
+
+        collisionDetectionCallback.onFrontCollision();
+    }
 }
