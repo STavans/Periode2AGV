944e1ac2cf9979508358978af95da534fc756063
MickWerf <mickwerf@outlook.com>
2019-11-28 08:03:04 +0100

    Added TestState Class for future use

---
+++ src/avg1a2/project/logic/TestState.java
@@ -0,0 +1,38 @@
+package avg1a2.project.logic;
+
+import java.util.ArrayList;
+
+public class TestState {
+    private ArrayList<String> states;
+    private String currentState;
+
+    public TestState() {
+        this.states = new ArrayList<>();
+    }
+
+
+    public void addState(String state) {
+        states.add(state);
+    }
+
+
+    public void setState(String state) throws IllegalArgumentException {
+        if (states.contains(state)) {
+            currentState = state;
+        } else {
+            throw new IllegalArgumentException("The requested state has not been defined.");
+        }
+    }
+
+    /**
+     * Getter function to get the current state, returns the state based on a boolean attribute.
+     * @return The current state of this object.
+     */
+    public String getState() throws IllegalStateException {
+        if (currentState != null) {
+            return currentState;
+        } else {
+         throw new IllegalStateException("No State has been set");
+        }
+    }
+}
