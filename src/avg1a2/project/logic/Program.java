761d3c9e68dcf5dcb969887b6f2909e0bcbfe642
MickWerf <mickwerf@outlook.com>
2019-12-05 12:21:38 +0100

    Added BluetoothSensor code Added BluetoothSensor callback code Added BliueBot Control code Edited Program to support bluetooth mode

--- src/avg1a2/project/logic/Program.java
+++ src/avg1a2/project/logic/Program.java
@@ -25,11 +25,18 @@ class Program {
      *                          or if the states aren't defined properly, the loop will exit with an Illegal State error.
      */
     void run() throws IllegalStateException{
+        boolean valid = false;
         while (running) {
-            if (dataStore.getProgramState().ifState("Override")) {
-                dataStore.getRemoteControl().run();
-            } else {
-                running = false;
+            switch (dataStore.getProgramState().getState()) {
+                case "Override" :
+                    dataStore.getRemoteControl().run();
+                    valid = true;
+                    break;
+                case "BlueBot" :
+                    valid = true;
+                    break;
+            }
+            if (!valid) {
                 throw new IllegalStateException("Program exited due to an illegal state.");
             }
             BoeBot.wait(1); //BoeBot wait 1 is required for the program to function and not disrupt itself, this is due to BoeBot behaviour.
--- src/avg1a2/project/modules/controller/BlueBotControl.java
+++ src/avg1a2/project/modules/controller/BlueBotControl.java
@@ -1,4 +1,25 @@
 package avg1a2.project.modules.controller;
 
-public class BlueBotControl {
+import avg1a2.project.hardware.sensor.bluetooth.BluetoothSensorCallback;
+import avg1a2.project.hardware.signal.led.NeoPixel;
+
+public class BlueBotControl implements BluetoothSensorCallback {
+    private NeoPixel neoPixel1;
+    private NeoPixel neoPixel2;
+
+    public BlueBotControl() {
+        neoPixel1 = new NeoPixel(0,0,0,255);
+        neoPixel2 = new NeoPixel(0,0,0,255);
+    }
+
+    @Override
+    public void onSignal(int data) {
+        if (data == 119) {
+            neoPixel1.on();
+            neoPixel2.on();
+        } else {
+            neoPixel1.off();
+            neoPixel2.off();
+        }
+    }
 }
