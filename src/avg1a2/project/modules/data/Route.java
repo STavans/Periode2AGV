2ded955b4d5156cf51a807ee9e5d46447b13c8e4
Shunian <kimveldhoen@hotmail.com>
2019-12-06 14:54:41 +0100

    start of new route

--- src/avg1a2/project/modules/data/Route.java
+++ src/avg1a2/project/modules/data/Route.java
@@ -1,4 +1,30 @@
 package avg1a2.project.modules.data;
 
+import java.util.ArrayList;
+
 public class Route {
+
+    private ArrayList<String> route;
+
+    public Route(){
+
+        this.route = new ArrayList<>();
+    }
+
+    public void setRoute(){
+
+        this.route.add("forward");
+        this.route.add("right");
+        this.route.add("forward");
+        this.route.add("forward");
+        this.route.add("forward");
+
+    }
+
+    public ArrayList<String> getRoute(){
+
+        return this.route;
+    }
+
+
 }
