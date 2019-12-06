95629d4dd515a0179a81e48af4df1f12ea0c05bc
MickWerf <mickwerf@outlook.com>
2019-12-06 12:37:53 +0100

    Added LineDetectionCallback functions and RouteControl attributes

--- src/avg1a2/project/modules/controller/RouteControl.java
+++ src/avg1a2/project/modules/controller/RouteControl.java
@@ -1,4 +1,42 @@
 package avg1a2.project.modules.controller;
 
-public class RouteControl {
+import avg1a2.project.hardware.sensor.linedetection.LineDetectionCallback;
+import avg1a2.project.modules.collisiondetection.CollisionDetection;
+import avg1a2.project.modules.collisiondetection.CollisionDetectionCallback;
+import avg1a2.project.modules.data.Route;
+
+public class RouteControl implements LineDetectionCallback, CollisionDetectionCallback {
+    private SignalControl signalControl;
+    private MotionControl motionControl;
+    private CollisionDetection collisionDetection;
+    private Route route;
+
+    public RouteControl() {
+
+    }
+
+    @Override
+    public void onCrossroads() {
+
+    }
+
+    @Override
+    public void lineCorrectionLeft() {
+
+    }
+
+    @Override
+    public void lineCorrectionRight() {
+
+    }
+
+    @Override
+    public void onLineLost() {
+
+    }
+
+    @Override
+    public void onFrontCollision() {
+
+    }
 }
