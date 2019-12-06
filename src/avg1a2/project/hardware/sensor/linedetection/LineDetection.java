78e5e24c048c03ae368a1c121f3c9ae7f7bc1a6e
MickWerf <mickwerf@outlook.com>
2019-12-06 16:12:19 +0100

    Fixed LineDetection, sort of

--- src/avg1a2/project/hardware/sensor/linedetection/LineDetection.java
+++ src/avg1a2/project/hardware/sensor/linedetection/LineDetection.java
@@ -6,54 +6,51 @@ import avg1a2.project.hardware.Component;
 
 public class LineDetection implements Component {
     private int threshhold;
-    private int backLeftSensor;
-    private int frontLeftSensor;
-    private int frontRightSensor;
+    private int leftSensor;
     private int backRightSensor;
+    private int midSensor;
+    private int frontRightSensor;
     private LineDetectionCallback callback;
     private Timer timer;
 
-    public LineDetection(int threshhold, int backLeftSensor, int frontLeftSensor, int frontRightSensor, int backRightSensor, LineDetectionCallback callback) {
+    public LineDetection(int threshhold, int leftSensor, int backRightSensor, int midSensor, int frontRightSensor, LineDetectionCallback callback) {
         this.threshhold = threshhold;
-        this.backLeftSensor = backLeftSensor;
-        this.frontLeftSensor = frontLeftSensor;
-        this.frontRightSensor = frontRightSensor;
+        this.leftSensor = leftSensor;
         this.backRightSensor = backRightSensor;
+        this.midSensor = midSensor;
+        this.frontRightSensor = frontRightSensor;
         this.callback = callback;
     }
 
     @Override
     public void update() {
         if (timer == null || timer.timeout()) {
-            System.out.println("Front left sensor is: " +  BoeBot.analogRead(frontLeftSensor));
-            System.out.println("Front right sensor is: " +  BoeBot.analogRead(frontRightSensor));
-//            System.out.println("Front back left sensor sensor is: " +  BoeBot.analogRead(backLeftSensor));
-//            System.out.println("Front back right sensor is: " +  BoeBot.analogRead(backRightSensor));
+            System.out.println("Left sensor is: " + BoeBot.analogRead(leftSensor));
+            System.out.println("Middle sensor is: " + BoeBot.analogRead(midSensor));
+            System.out.println("Right sensor is: " + BoeBot.analogRead(frontRightSensor));
+            System.out.println("Back right sensor is: " + BoeBot.analogRead(backRightSensor));
+            timer = new Timer(50);
+        }
 
-            int dataFrontLeft = BoeBot.analogRead(frontLeftSensor);
-            int dataFrontRight = BoeBot.analogRead(frontRightSensor);
-            int dataBackLeft = BoeBot.analogRead(backLeftSensor);
-            int dataBackRight = BoeBot.analogRead(backRightSensor);
+        int dataLeftSensor = BoeBot.analogRead(leftSensor);
+        int dataMidSensor = BoeBot.analogRead(midSensor);
+        int dataFrontRightSensor = BoeBot.analogRead(frontRightSensor);
+        int dataBackRightSensor = BoeBot.analogRead(backRightSensor);
 
-
-
-
-            if(dataFrontLeft > threshhold && dataFrontRight > threshhold && dataBackLeft > threshhold && dataBackRight > threshhold){
-                callback.onCrossroads();
-            }
-            if(dataFrontLeft < threshhold){
-                callback.lineCorrectionRight();
-            }
-            if(dataFrontRight < threshhold){
-                callback.lineCorrectionLeft();
-            }
-            if(dataFrontLeft > threshhold && dataFrontRight > threshhold){
-                callback.goForward();
-            }
-            if(dataFrontLeft < threshhold && dataFrontRight < threshhold && dataBackLeft < threshhold && dataBackRight < threshhold){
-                callback.onLineLost();
-            }
-            timer = new Timer(2000);
+        if (dataMidSensor > threshhold && dataBackRightSensor > threshhold) {
+            callback.onCrossroads();
+        }
+        if (dataLeftSensor > threshhold && dataFrontRightSensor < threshhold) {
+            callback.lineCorrectionLeft();
+        }
+        if (dataFrontRightSensor > threshhold && dataLeftSensor < threshhold) {
+            callback.lineCorrectionRight();
+        }
+        if (dataMidSensor > threshhold) {
+            callback.goForward();
+        }
+        if (dataMidSensor < threshhold && dataFrontRightSensor < threshhold && dataBackRightSensor < threshhold && dataLeftSensor < threshhold && midSensor < dataMidSensor) {
+            callback.onLineLost();
         }
     }
 }
