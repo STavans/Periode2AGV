9deae1d2c3ae8dd300d01d5bc8dd731833e227ff
MickWerf <mickwerf@outlook.com>
2020-01-11 21:22:19 +0100

    BluetoothConversion: Added comments adn merged cases with the same result. BluetoothConversionCallback: Added comments. BlueBotControl: Added comments. RouteControl: Added comments. Datastore: Added comments and removed WarningSpeaker Route: Added comments and removed unused clearSteps function

--- src/avg1a2/project/modules/data/Route.java
+++ src/avg1a2/project/modules/data/Route.java
@@ -2,18 +2,30 @@ package avg1a2.project.modules.data;
 
 import java.util.ArrayList;
 
+/**
+ * Class which defines and manages the functions of a route.
+ */
 public class Route {
     private ArrayList<String> steps;
     private int currentStep;
     private boolean complete;
     private boolean finished;
 
+    /**
+     * Constructor which initializes the attributes except for currentStep.
+     */
     public Route(){
         this.steps = new ArrayList<>();
         this.complete = false;
         this.finished = false;
     }
 
+    /**
+     * Function which adds a step to the current route.
+     * @param step Step which to add.
+     * @throws IllegalArgumentException Throws error when the given step is not a defined, possible step.
+     * @throws IllegalStateException Throws error if the route is finished and a step is still trying to be added.
+     */
     public void addStep(String step) throws IllegalArgumentException, IllegalStateException {
         if (!complete) {
             if (step.equals("Left") || step.equals("Right") || step.equals("Forward") || step.equals("Stop")) {
@@ -29,6 +41,11 @@ public class Route {
         }
     }
 
+    /**
+     * Method used to request the next step.
+     * @return String version of the next step listed in the route.
+     * @throws IllegalStateException Throws and exception if the route has no end defined, has already finished, or if there is an error with the route itself in the arrayList.
+     */
     public String nextStep() throws IllegalStateException {
         if (!complete) {
             throw new IllegalStateException("The route does not have an endpoint defined!");
@@ -46,13 +63,11 @@ public class Route {
         return step;
     }
 
+    /**
+     * Resets the route back to step 1, meaning a route can be used multiple times.
+     */
     public void reset() {
         this.finished = false;
         this.currentStep = 0;
-    }
-
-    public void clearSteps() {
-        this.steps = new ArrayList<>();
-        this.complete = false;
     }
 }
